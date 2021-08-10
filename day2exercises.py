8989e3a7369675ff6414985b1acc4e7db0acaabb
Owen Carvill <88054323+carvillain@users.noreply.github.com>
2021-08-10 15:44:06 -0500

    Assignments first commit

---
+++ day2exercises.py
@@ -0,0 +1,45 @@
+# Function Exercise
+
+# first_name = ['John', 'Evan', 'Jordan', 'Max']
+# last_name = ['Smith', 'Smith', 'Williams', 'Bell']
+
+# new_list = []
+# def build_name(first_name, last_name):
+#     zip1 = zip(first_name, last_name)
+#     for first, last in zip1:
+#         full = first + " " + last
+#         new_list.append(full)
+#     return new_list
+        
+# build_name(first_name, last_name)
+
+
+
+# Less than 10 function
+
+# l_1 = [1,11,14,5,8,9,11,12]
+# l_2 = []
+
+# def less_than_ten(l_1):
+#     for x in l_1:
+#         if x >= 10:
+#             continue
+#         else:
+#             l_2.append(x)
+#     return l_2
+        
+# less_than_ten(l_1)
+
+
+
+# Merge and sort two lists
+
+# l_1 = [1,2,3,4,5,6]
+# l_2 = [3,4,5,6,7,8,10]
+
+# def combine(l_1, l_2):
+#     longlist = l_1 + l_2
+#     longlist.sort()
+#     return longlist
+
+# combine(l_1, l_2)
