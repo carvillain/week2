6fd030ae3a1219d432056694c1b971cf615144f6
carvillain <owen.carvill@gmail.com>
2021-08-10 14:17:07 -0500

    reupload

---
+++ assignments.py
@@ -0,0 +1,26 @@
+# Printing prime numbers
+# for prime in range(2, 101):
+#     for i in range(2,prime):
+#         if prime % i == 0:
+#             break
+#     else:
+#         print(prime)
+
+
+# Print out all cubed numbers up to total value 1000
+# for i in range(15):
+#     if i**3 > 1000:
+#         break
+#     else:
+#         print(i**3)
+
+# Take in a users input for their age, if they are younger than 18 print kids, if they're 18 to
+# 65 ring adults, else print seniors
+
+# age = int(input("Please enter your age: "))
+# if age < 18:
+#     print("kids")
+# elif age >= 18 and age < 65:
+#     print("adults")
+# else:
+#     print("seniors")
